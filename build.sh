e26897a4ae2cd113e147dcbf5b1b5ce20cbc2068
Mike Morris <michael.m.morris@gmail.com>
2016-10-14 09:45:02 -0500

    cleaned up build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,4 @@
 #!/bin/bash -x
-#  1. `cd pre-install` and `docker build -t snm:base .` to do the basic system setup
-#  2. Run an interactive container from the resulting image (docker run -it --name snm-build snm:base)
-#    A. execute the snm install.pl script (cd /tmp/snm && perl install.pl) and answer the questions (defaults should be fine)
-#    B. delete the un-archived snm installation files (cd / && rm -rf /tmp/snm)
-#  3. exit the container
-#  4. `cd ..` and do a `docker export -o snm.tar snm-build` to dump the container filesystem
-#  5. Run `docker build -t snm:latest .` to build the final image
-#  6. Run a container from the resulting image and test (see example below)
-#  7. Clean up the 'pre-install' image and container, and the snm.tar file created from the `docker export` (rm snm.tar; docker rm snm-build; docker rmi snm:base)
 
 script_dir=$(readlink -f `dirname $0`)
 
